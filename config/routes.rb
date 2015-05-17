83b589a86160c087f133e651df28073868a16e60
Jeff Shapiro <codesheff@gmail.com>
2015-05-16 20:26:50 -0400

    routes edit for heroku 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   resources :order_profiles
   resources :users
 
-  get "/"
-  "users#index"
+  root "users#index"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
