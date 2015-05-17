ae56a94ae0a87015cfa8f96cf3b7ca45e89bbb4a
Joshua Echeverri <josheche@gmail.com>
2015-05-17 13:40:37 -0400

    commented out has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :order_profiles
-  has_secure_password
+  # has_secure_password
 end
