6e49f5ab5f9ade0e6c182e2cbb4d8d432c271538
Jeff Shapiro <codesheff@gmail.com>
2015-05-16 20:25:20 -0400

    fixing routes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :coffee_shops
   resources :order_profiles
   resources :users
+
+  get "/"
+  "users#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
