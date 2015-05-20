f51935aa68cb03fb4974ba19a0106f42ba0a052a
Maximo Rosario <xam527@gmail.com>
2015-05-20 17:58:16 -0400

    Final changes to text_controllers need action to test seding a text from app

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -2,24 +2,38 @@ class ReceiveTextController < ApplicationController
   # require 'rubygems'
   # require 'twilio-ruby'
 
-  def index
-    #need to add routes localhost::3000/sms to routes
 
-    account_sid = "ACbf9d315e805714fbda6f3bda0a472f11"
-    auth_token = "55edd5e9b140e907a7b7e7b1ec4a977a"
 
-    message_body = params["Body"]
-    from_number = params["From"]
 
-    @client = Twilio::REST::Client.new account_sid, auth_token
 
-    @order_message =  @client.account.sms.messages.list({}).each do |sms|
-                        puts sms.to
-                      end
+  # def sms
+  #   sender = params[:From]
+  #   body = params[:Body]
+  #   @order_confirmation = Chirp.new(from: sender, description: body)
+  #   @chirp.save
+  # end
 
-    @sms = @client.account.sms.messages.get("SM1d2c461018a3245defd8589527bd8c98")
 
-  end
+
+
+  # def index
+  #   #need to add routes localhost::3000/sms to routes
+
+  #   account_sid = "ACbf9d315e805714fbda6f3bda0a472f11"
+  #   auth_token = "55edd5e9b140e907a7b7e7b1ec4a977a"
+
+  #   message_body = params["Body"]
+  #   from_number = params["From"]
+
+  #   @client = Twilio::REST::Client.new account_sid, auth_token
+
+  #   @order_message =  @client.account.sms.messages.list({}).each do |sms|
+  #                       puts sms.to
+  #                     end
+
+  #   @sms = @client.account.sms.messages.get("SM1d2c461018a3245defd8589527bd8c98")
+
+  # end
 
         #    do  sender = params[:From]
         #   friends = {
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'receive_text/index'
+  get 'twilio/sms', defaults: { format: 'twiml' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
