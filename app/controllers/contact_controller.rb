b1a3b1a66954404c149df40499b51302834498bb
Jeff Shapiro <codesheff@gmail.com>
2015-05-23 19:31:57 -0400

    updated contact controller

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,2 +1,31 @@
 class ContactController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @create = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = 'Message delivered'
+    else
+      flash.now[:error] = 'Cannot send message'
+      render :new
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
