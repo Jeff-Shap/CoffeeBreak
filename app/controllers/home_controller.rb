50648094baf37a4e61a23ff7a1283700be1a2738
Jeff Shapiro <codesheff@gmail.com>
2015-05-28 16:07:27 -0400

    added new order event to home/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 class HomeController < ApplicationController
+  
   def index
+    @order=Order.new
+    @order_json = @order.to_json
   end
+  
+
   def get_coffee
     TwilioClient.send_text_message
     redirect_to home_index_path, notice: 'A runner is on their way!'
   end
+
+
 end
