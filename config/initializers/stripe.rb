cf5ff4b6630621c8865302509fb7394c8042b97c
Maximo Rosario <xam527@gmail.com>
2015-05-30 14:39:55 -0400

    added change to schema, I did not make the change, name removed from users, added test stripe keys to initializer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,9 @@
-# Rails.configuration.stripe = {
-#   :publishable_key => ENV['PUBLISHABLE_KEY'],
-#   :secret_key      => ENV['SECRET_KEY']
-# }
+Rails.configuration.stripe = {
+  :publishable_key => sk_test_WihPuQmGuaVRXNGHx5Z8OZHd,
+  :secret_key      => pk_test_g0hdgorjGBmKlEdJ1sJIe5ev
+}
 
-# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
-Stripe.api_key = secret_key
-STRIPE_PUBLIC_KEY = public_key
+# Stripe.api_key = secret_key
+# STRIPE_PUBLIC_KEY = public_key
