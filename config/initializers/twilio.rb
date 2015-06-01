7d805d6d806b7fac9258574ef6b283a2f44221fb
Maximo Rosario <xam527@gmail.com>
2015-06-01 17:23:12 -0400

    added .env-example, hid api keys for twilio

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,18 +1,4 @@
-if Rails.env.test? || Rails.env.development?
-  #auth_token = "83f89618e1c835b7e6602c1b260507c4"
-  auth_token = "8c68a13a80c6f7036c70ce5fedb4f27c"
-else
-  auth_token = ENV["AUTH_TOKEN"]
-end
-
-if Rails.env.test? || Rails.env.development?
-  #account_sid = "AC9d4c46c95f181d6e03c1a89fca868bc3"
-  account_sid = "AC3a0b68aae943d7866406c90000665ac3"
-else
-  account_sid = ENV["ACCOUNT_SID"]
-end
-
 Twilio.configure do |config|
-  config.account_sid = "AC3a0b68aae943d7866406c90000665ac3"
-  config.auth_token = "8c68a13a80c6f7036c70ce5fedb4f27c"
+  config.account_sid = ENV["ACCOUNT_SID"]
+  config.auth_token = ENV["AUTH_TOKEN"]
 end
